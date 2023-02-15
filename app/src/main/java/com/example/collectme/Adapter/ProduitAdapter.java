d740514d001e96122353286921260807e1d1dd5c
Remi <urbaniec.remi@gmail.com>
2023-02-15 11:21:23 +0100

    exemple RecyclerView

---
+++ app/src/main/java/com/example/collectme/Adapter/ProduitAdapter.java
@@ -0,0 +1,67 @@
+package com.example.collectme.Adapter;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.collectme.Entity.Produit;
+import com.example.collectme.R;
+
+import java.util.ArrayList;
+
+public class ProduitAdapter extends RecyclerView.Adapter<ProduitAdapter.ProduitViewHolder> {
+
+    private ArrayList<Produit> listProduits;
+
+    public ProduitAdapter(ArrayList<Produit> listProduits) {
+        this.listProduits = listProduits;
+    }
+
+    @NonNull
+    @Override
+    public ProduitAdapter.ProduitViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        /// On va crée une vue qui à besoin du context ici : ListeProduitActivity, pour "gonfler" c'est à dire
+        //Ajouter un composant qui n'est pas encore définit dans notre vue. Pour cela il a besoin de la vue que l'on souhaite ajouté et du
+        // parent donc du recyclerview
+        View v  = LayoutInflater.from(parent.getContext()).inflate(R.layout.ligne_produit,parent,false);
+        ProduitViewHolder produitViewHolder = new ProduitViewHolder(v);
+        return produitViewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ProduitAdapter.ProduitViewHolder holder, int position) {
+        //on récupére le produit de la ligne concerné (postion), et on set les composants avec les bonne données
+        Produit produitEncours = listProduits.get(position);
+        holder.tv_nomArticle.setText(produitEncours.getNomProduit());
+        holder.tv_descriptionArticle.setText(produitEncours.getDescriptionProduit());
+        holder.tv_prixHT.setText(String.valueOf(produitEncours.getPrixHt() + " €"));
+        holder.tv_prixTTC.setText(String.valueOf(produitEncours.getPrixTtc()+ " €"));
+    }
+
+    @Override
+    public int getItemCount() {
+        // on renseigne le nombre de ligne pour qu'il puisse les gérées
+        return listProduits.size();
+    }
+
+    public class ProduitViewHolder extends RecyclerView.ViewHolder {
+        //On initialise les composants de la ligne
+        public TextView tv_nomArticle;
+        public TextView tv_descriptionArticle;
+        public TextView tv_prixHT;
+        public TextView tv_prixTTC;
+        public ProduitViewHolder(@NonNull View itemView) {
+            super(itemView);
+            //On récupérer les composants de la ligne
+            tv_nomArticle = itemView.findViewById(R.id.tv_nomArticle);
+            tv_descriptionArticle = itemView.findViewById(R.id.tv_description);
+            tv_prixHT = itemView.findViewById(R.id.tv_prixHt);
+            tv_prixTTC = itemView.findViewById(R.id.tv_prixTTC);
+            //Ici on pourra ajouter l'evenement onclick sur la ligne
+        }
+    }
+}
--- app/src/main/java/com/example/collectme/ListeProduitsActivity.java
+++ app/src/main/java/com/example/collectme/ListeProduitsActivity.java
@@ -1,12 +1,15 @@
 package com.example.collectme;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
+import com.example.collectme.Adapter.ProduitAdapter;
 import com.example.collectme.Entity.Produit;
 import com.example.collectme.Managers.ProduitManager;
 
@@ -14,8 +17,12 @@ import java.util.ArrayList;
 
 public class ListeProduitsActivity extends AppCompatActivity {
 
-
+    //initialisation des composants
     private Spinner sp_listproduit;
+    private RecyclerView rv_listeproduit;
+    private ProduitAdapter produitAdapter;
+    private RecyclerView.LayoutManager layoutManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,7 +31,9 @@ public class ListeProduitsActivity extends AppCompatActivity {
         getAllproduit();
     }
     public void init(){
+        //on récupère depuis la vue les composant pour pouvoir intéragir avec.
         sp_listproduit = findViewById(R.id.sp_listeproduit);
+        rv_listeproduit = findViewById(R.id.rv_listeProduits);
     }
     public void getAllproduit(){
         ProduitManager pm =  new ProduitManager();
@@ -33,6 +42,7 @@ public class ListeProduitsActivity extends AppCompatActivity {
     public void getAllProduitComplete(ArrayList<Produit> listProduits){
 
         remplirSpinner(listProduits);
+        remplirRecyclerView(listProduits);
     }
     public void remplirSpinner(ArrayList<Produit> listProduits){
         ArrayList<String> listesproduitsNom = new ArrayList<>();
@@ -44,6 +54,21 @@ public class ListeProduitsActivity extends AppCompatActivity {
         }
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item,listesproduitsNom);
         sp_listproduit.setAdapter(adapter);
+
+
+
+    }
+    public void remplirRecyclerView(ArrayList<Produit> listProduits){
+        // Gestion du recylerView
+        //on renseigne la liste des produits à notre Adapter
+        produitAdapter = new ProduitAdapter(listProduits);
+
+        layoutManager = new LinearLayoutManager(this);
+        rv_listeproduit.setLayoutManager(layoutManager);
+
+        //On relie l'adapter au re
+        rv_listeproduit.setAdapter(produitAdapter);
+
 
     }
 }
